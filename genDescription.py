466281079cd3525179a20fc12317f3f600f9ef78
Bear19997777 <jerry880915@gmail.com>
2024-07-29 22:51:38 +0800

    update description

--- genDescription.py
+++ genDescription.py
@@ -2,15 +2,14 @@ import databaseOp as dbo
 import requests
 from bs4 import BeautifulSoup
 import html_text
+from deep_translator import GoogleTranslator
 # 定義要獲取的URL
 
 
 def extractWebsiteText():
-    dbobj = dbo.ShopifyDatabase()
+    dbobj = dbo.ShopifyDatabase("./database/database.xlsx")
     dbDf = dbobj.GetDatabase()
-    print(dbDf)
     urls = dbDf["url"]
-    print(urls)
     for url in urls : 
         print(url)
         # 發送HTTP GET請求
@@ -19,12 +18,18 @@ def extractWebsiteText():
             html_content = response.text
             soup = BeautifulSoup(html_content, 'html.parser')
             # 打印解析後的HTML
-            print(soup.prettify())
         else:
             print(f"Failed to retrieve the URL: {response.status_code}")
         
         extract = html_text.extract_text(soup.prettify(),guess_layout=False)
-        print(extract)
-    return extract
+        translator = GoogleTranslator(source='auto', target='en')
+        text = "".join([translator.translate(extract[i*4000:(i+1)*4000]) for i in range(len(extract)//4000)])
+        
+        print(text)
+    return text
 
+
+if __name__ == "__main__":
+    text = extractWebsiteText()
+    
     
